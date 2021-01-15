fd150f59e77b85dfe44cd875e7dbae93ec094ee1
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-15 13:01:16 -0500

    added custom express error

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 app.use(express.json())
+const ExpressError = require("./expressError")
 
-
+// routes
 app.get("/mean", (req, res) => {
+  try {
     let nums = req.query.nums
     const calc = new Calculator("mean", nums);
     calc.mean()
     return res.send(calc.response())
+  } catch(e) {
+    next(e)
+  }
 })
 
 app.get("/median", (req, res) => {
@@ -17,10 +22,31 @@ app.get("/median", (req, res) => {
   return res.send(calc.response())
 })
 
+app.get("/mode", (req, res) => {
+  let nums = req.query.nums
+  const calc = new Calculator("mode", nums);
+  calc.mode()
+  return res.send(calc.response())
+})
+
+// error handler
+app.use((error, req, res, next) => {
+  let status = error.status || 500;
+  let message = error.msg;
+
+  return res.status(status).json({
+    error: {message, status}
+  })
+})
+
 app.listen(3000, () => {
     console.log("Running on port 3000")
 })
 
+
+// Calculator calls mean, median, mode
+// checks to see if all numbers passed in or throws error
+// method for response
 class Calculator {
   constructor(operation, nums) {
     this.operation = operation;
@@ -35,7 +61,7 @@ class Calculator {
     this.nums = [];
     testArray.forEach((el)=> {
       if (Number.isNaN(el)) {
-        throw new Error("ERROR: All inputs must be a valid number")
+        throw new ExpressError("ERROR: All inputs must be a valid number", 403)
       }  
       this.nums.push(el)
     })
@@ -52,14 +78,14 @@ class Calculator {
 }
 
   median() {
-    let arr = this.nums.map(el => parseInt(el));
+    let arr = this.nums;
     const mid = Math.floor(arr.length / 2),
     nums = [...arr].sort((a, b) => a - b);
     this.value = arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
     return this.value;
 }
   mode() {
-    let arr = this.nums.map(el => parseInt(el));
+    let arr = this.nums;
     let obj = arr.reduce(function (acc, curr) {
         if (typeof acc[curr] == 'undefined') {
           acc[curr] = 1;
@@ -72,7 +98,6 @@ class Calculator {
       this.value = Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
     return this.value;
 }
-
   response() {
     let response = {
       operation: this.operation,
@@ -82,34 +107,3 @@ class Calculator {
     // return {"response": {"operation": `${this.operation}`, "value": `${this.value}`}}
 }
 }
-
-
-
-
-
-// // 
-// function mean(nums) {
-//   let sum = 0;
-//   for (let i=0; i < nums.length; i++) {
-//       sum = sum + nums[i];
-//   }
-//   return (sum / nums.length)
-// }
-
-// function median(arr) {
-//   const mid = Math.floor(arr.length / 2),
-//   nums = [...arr].sort((a, b) => a - b);
-// return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-// };
-
-// function mode(arr) {
-//   let obj = arr.reduce(function (acc, curr) {
-//       if (typeof acc[curr] == 'undefined') {
-//         acc[curr] = 1;
-//       } else {
-//         acc[curr] += 1;
-//       }
-//       return acc;
-//     }, 
-//     {});
-//   return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
